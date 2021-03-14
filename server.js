437475d7b6eb7f221314579bc8cefec7af5d1055
Temch4k <artsiom.skarakhod@gmail.com>
2021-03-14 16:46:04 -0600

    Update server.js

--- server.js
+++ server.js
@@ -186,7 +186,7 @@ router.route('/moviecollection')
                     throw err;
                 }
                 // if movie is null then we never found the movie we were looking for
-                else if(movie.n == null)
+                else if(movie.n === 0)
                 {
                     return res.status(403).json({success: false, message: "Error, can't find the movie"});
                     throw err;
